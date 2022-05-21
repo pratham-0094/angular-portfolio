bcf027503764fef99e77530e8d6e9f9a12e672b9
pratham-0094 <sahupratham022003@gmail.com>
2022-05-21 17:40:12 +0530

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        white: "#FFFFFF",
+        black: "#000000",
+        "silver-chalice": "#B3B3B3",
+        "lapis-lazuli": "#325FA4",
+        "maximu-blue": "#39ABCF",
+      },
+    },
   },
   plugins: [],
 };
