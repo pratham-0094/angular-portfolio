0995d3a6e4f3552d4f407a88fa66f23f11f6def5
pratham-0094 <sahupratham022003@gmail.com>
2022-05-22 19:40:22 +0530

    navbar responsive completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-portfolio';
 
-  constructor(public Location: Location) {}
+  constructor(public Location: Location, private router: Router) {
+    if (this.Location.path() === '') {
+      this.router.navigateByUrl('/Home');
+    }
+  }
+
+  next() {
+    let list = document.querySelectorAll('.navbar ul li');
+    if (list[0].classList.contains('active')) {
+      document.getElementById('About')?.click();
+    } else if (list[1].classList.contains('active')) {
+      document.getElementById('Project')?.click();
+    } else if (list[2].classList.contains('active')) {
+      document.getElementById('Work')?.click();
+    } else if (list[3].classList.contains('active')) {
+      document.getElementById('Contact')?.click();
+    }
+  }
+
+  prev() {
+    let list = document.querySelectorAll('.navbar ul li');
+    if (list[1].classList.contains('active')) {
+      document.getElementById('Home')?.click();
+    } else if (list[2].classList.contains('active')) {
+      document.getElementById('About')?.click();
+    } else if (list[3].classList.contains('active')) {
+      document.getElementById('Project')?.click();
+    } else if (list[4].classList.contains('active')) {
+      document.getElementById('Work')?.click();
+    }
+  }
 }
