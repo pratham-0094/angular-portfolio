c09cabbb8e4d3d1d1e846a69f059d888979af975
pratham-0094 <sahupratham022003@gmail.com>
2022-05-24 14:45:26 +0530

    add insta logo

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
-  styleUrls: ['./work.component.css']
+  styleUrls: ['./work.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(750, style({ opacity: 1 })),
+      ]),
+      transition('* => void', [
+        style({ opacity: 1 }),
+        animate(750, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class WorkComponent implements OnInit {
+  media = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.onResize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth > 700) {
+      this.media = true;
+    } else {
+      this.media = false;
+    }
+  }
+
+  ngOnInit(): void {}
 }
