fc591113afeddb943eb1dd95252dd0d421466235
pratham-0094 <sahupratham022003@gmail.com>
2022-05-24 13:30:39 +0530

    contact page complete

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(750, style({ opacity: 1 })),
+      ]),
+      transition('* => void', [
+        style({ opacity: 1 }),
+        animate(750, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class ContactComponent implements OnInit {
+  media = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.onResize();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    if (window.innerWidth > 700) {
+      this.media = true;
+    } else {
+      this.media = false;
+    }
+  }
+
+  ngOnInit(): void {}
 }
